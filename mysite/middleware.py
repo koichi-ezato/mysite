0077ffee8feb9fe9bfafa19fc6b41a72a8138792
Koichi Ezato <koichi.e.38@gmail.com>
2020-03-31 05:08:49 +0900

    ミドルウェア実装

---
+++ mysite/middleware.py
@@ -0,0 +1,20 @@
+# middleware.py
+class SampleMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # 前処理
+        self.process_request(request)
+        # ビューの処理
+        response = self.get_response(request)
+        # 後処理
+        self.process_response(request, response)
+
+        return response
+
+    def process_request(self, request):
+        print("リクエスト")
+
+    def process_response(self, request, response):
+        print("レスポンス")
